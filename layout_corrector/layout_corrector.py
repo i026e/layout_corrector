7d5d1efa786b685daee4dcf41bd808088aa8d412
i026e <klev.paul@gmail.com>
2017-01-26 19:34:31 +0300

    Bug fix

--- layout_corrector/layout_corrector.py
+++ layout_corrector/layout_corrector.py
@@ -30,96 +30,103 @@ def key_combo(keyboard, modifier, key):
     keyboard.press(key)
     keyboard.release(key)
     keyboard.release(modifier)
-        
+
 def cut_to_buffer(keyboard):
     key_combo(keyboard, *config.cut_combo)
-    
+
 def paste_from_buffer(keyboard):
     key_combo(keyboard, *config.paste_combo)
-    
+
 def get_buffer():
     #pyperclip
     p = subprocess.Popen(['xsel', '-b', '-o'],
                          stdout=subprocess.PIPE, close_fds=True)
     stdout, stderr = p.communicate()
     return stdout.decode('utf-8')
-    
+
 def set_buffer(text):
     p = subprocess.Popen(['xsel', '-b', '-i'],
                              stdin=subprocess.PIPE, close_fds=True)
     p.communicate(input=text.encode('utf-8'))
 
 def play(sound):
-    if config.sounds and sound is not None:
-        subprocess.Popen(["aplay", sound],)  
+    if config.sounds:
+	    try:
+            p = subprocess.Popen(["aplay", sound],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, close_fds=True)
+            stdout, stderr = p.communicate()
+            print(stdout.decode('utf-8'))
+        except Exception as e:
+            print(e)
 
 class Switcher:
     def __init__(self):
         self.xkb = XKeyboard()
         self.controller = Controller()
         self.state = state.StateMachine()
-        
+
         self.busy = False
-        
-    def run(self):        
+
+    def run(self):
         with Listener(on_press = self.on_key_press,
                       on_release = self.on_key_release) as listener:
             listener.join()
-       
+
 
     def announce_key(self):
-        group = self.xkb.group_num    
+        group = self.xkb.group_num
         play(group_sounds.get(group))
-        
+
 
     def on_key_press(self, key):
-        print("press", key)        
+        print("press", key)
         self.state.press(key)
-        
+
         try:
             char = key.char
             self.announce_key()
-        except:
+        except Exception as e:
             if key in config.sound_keys:
                 self.announce_key()
 
-    
+
     def on_key_release(self, key):
         print("release", key)
         if self.state.bingo():
             self.replace_selection()
-        self.state.release(key)   
-        
-    def get_text(self):         
+        self.state.release(key)
+
+    def get_text(self):
         cut_to_buffer(self.controller)
-        
+
         text = get_buffer()
-        print("get text", text)   
+        print("get text", text)
         return text
-    
+
     def set_text(self, text):
         print("set_text", text)
-        set_buffer(text)    
+        set_buffer(text)
         paste_from_buffer(self.controller)
         set_buffer("")
 
     def replace_selection(self):
         if not self.busy:
-            self.busy = True  
+            self.busy = True
             print("replacing")
-            
+
             for key in self.state.pressed_keys():
                 self.controller.release(key)
-        
+
             text = self.get_text()
-            
+
             if len(text) > 0:
                 text = correction.correct(text)
-                
-                self.set_text(text)    
+
+                self.set_text(text)
                 play(config.replace_sound)
-                
+
             self.busy = False
-    
+
 if __name__ == '__main__':
     Switcher().run()
